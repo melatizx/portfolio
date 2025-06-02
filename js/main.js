813d42187deb99096ea179a4221649443835d1db
melatizx <leo.ambrosio0107@gmail.com>
2025-06-02 18:34:50 -0300

    .

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ function trocar(imagemElement, novaImagem){
     imagemElement.classList.toggle("imgEfeito");
     imagemElement.src = novaImagem 
 }
+
+function enviarEmail(){}
+emailjs.init("dyjrDHda6rLlc81fS")
+    document.getElementById('emailForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const form = event.target;
+        const formData = new FormData(form);
+
+        emailjs.sendForm('service_mrbmjwk', 'template_qrcptw4', form)
+        .then(function(response) {
+            alert('E-mail enviado com sucesso!');
+        }, function(error) {
+            alert('Erro ao enviar o e-mail: ' + error.text);
+        });
+    });
