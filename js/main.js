50d6579c54915aba7834af394d1d2cc01bc08766
melatizx <leo.ambrosio0107@gmail.com>
2025-06-09 20:53:39 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-// Enviar email
-function enviarEmail(){
-    let assuntoInserido = document.getElementById('assuntoInserido').value
-    let corpoInserido = document.getElementById('corpoInserido').value
-    const destinatario = 'leonardomelati1@gmail.com'
-    const assunto = encodeURIComponent(assuntoInserido);
-    const corpo = encodeURIComponent(corpoInserido);
-    const url = `https://mail.google.com/mail/?view=cm&fs=1&to=${destinatario}&su=${assunto}&body=${corpo}`
-    window.open(url, '_blank')
-}
+// // Enviar email
+// function enviarEmail(){
+//     let assuntoInserido = document.getElementById('assuntoInserido').value
+//     let corpoInserido = document.getElementById('corpoInserido').value
+//     const destinatario = 'leonardomelati1@gmail.com'
+//     const assunto = encodeURIComponent(assuntoInserido);
+//     const corpo = encodeURIComponent(corpoInserido);
+//     const url = `https://mail.google.com/mail/?view=cm&fs=1&to=${destinatario}&su=${assunto}&body=${corpo}`
+//     window.open(url, '_blank')
+// }
 
 //Trocar imagem
 function trocar(imagemElement, novaImagem){
