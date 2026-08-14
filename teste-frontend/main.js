46be9638b1eec1c009b94540de92acf376e6ed6d
Leonardo Melati <leonardomelati1@gmail.com>
2026-08-14 13:28:42 -0300

    .

--- teste-frontend/main.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-toggle.addEventListener("click", () => {
-    const isOpen = navLinks.classList.toggle("active");
-    toggle.setAttribute("aria-expanded", isOpen);
-  });
